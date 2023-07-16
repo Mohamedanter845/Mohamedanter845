2911694f0300d4a1ef2528c19a93b58431cd16c9
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 19:35:58 +0200

    2

--- assignment1.ex1/src/ex6.c
+++ assignment1.ex1/src/ex6.c
@@ -5,4 +5,21 @@
  *      Author: Anter
  */
 
+#include <stdio.h>
+#include <stdio.lb>
+int main(void)
 
+{
+    float a,b,c;
+    printf("enter a value of  a:\n");
+    fflush(stdin);fflush(stdout);
+    scanf("%f",&a);
+    printf("enter a valua of b :\n");
+    fflush(stdin);fflush(stdout);
+    scanf("%f",b);
+    c =a;
+    a =b;
+    b =c;
+    printf("\nAfter swapping, value of a = %f",a);
+	printf("\nAfter swapping, value of b = %f",b);
+}
--- assignment1.ex1/src/ex7.c
+++ assignment1.ex1/src/ex7.c
@@ -6,3 +6,24 @@
  */
 
 
+#include "stdio.h"
+
+void main()
+{
+	int a,b;
+	printf("Enter value of a: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d",&a);
+
+	printf("Enter value of b: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d",&b);
+
+	a=a*b;
+	b=a/b;
+	a=a/b;
+
+	printf("\nAfter swapping, value of a = %d",a);
+	printf("\nAfter swapping, value of b = %d",b);
+
+}
