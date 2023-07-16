98b738655c0054e59d52448724ee5f4b9d25539a
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 19:38:13 +0200

    8

--- ex2/src/EX8.c
+++ ex2/src/EX8.c
@@ -6,3 +6,43 @@
  */
 
 
+#include <stdio.h>
+
+void main()
+{
+	char o;
+	float num1, num2, answer;
+
+
+	printf("Enter operator either + or - or * or divide : ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%c",&o);
+
+	printf("Enter two operands: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%f %f", &num1, &num2);
+
+	switch(o)
+	{
+	case '+':
+		printf("%f +%f=%f",num1,num2,num1+num2);
+		break;
+	case '-':
+		printf("%f -%f=%f",num1,num2,num1-num2);
+
+		break;
+	case '*':
+		printf("%f *%f=%f",num1,num2,num1*num2);
+
+		break;
+	case '/':
+	    printf("%f /%f=%f",num1,num2,num1/num2);
+
+		break;
+	default:
+		printf("error");
+
+		break;
+	}
+
+}
