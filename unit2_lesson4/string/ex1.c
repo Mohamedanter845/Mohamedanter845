412cb319ef322eaa336e79f19138f53fea0c9ed0
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 19:52:59 +0200

    1

--- unit2_lesson4/string/ex1.c
+++ unit2_lesson4/string/ex1.c
@@ -6,3 +6,26 @@
  */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i,x=0;
+	char a[100],ch;
+	printf("Enter the sentence:");
+	fflush(stdin);fflush(stdout);
+	gets(a);
+	printf("Enter the letter to get the frequency:");
+	fflush(stdin);fflush(stdout);
+	scanf("%c",&ch);
+	for (i=0;a[i]!='\0';i++)
+	{
+		if(ch==a[i])
+			++x;
+	}
+
+	printf("the frequency of chacter %c is %d",ch,x);
+	return(0);
+
+
+}
