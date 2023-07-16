4976a32802407915dc144e8a9033fc0c3464e0ec
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 18:45:01 +0200

    third

--- assignment1.ex1/src/ex3.c
+++ assignment1.ex1/src/ex3.c
@@ -5,4 +5,14 @@
  *      Author: Anter
  */
 
+#include <stdio.h>
 
+void main()
+{
+	int x, y,result;
+	printf("Enter two integers to add: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d %d", &x, &y);
+    result=x+y;
+	printf("Sum is : %d", result);
+}
