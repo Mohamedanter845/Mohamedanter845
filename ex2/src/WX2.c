bf1aaefd1692de4c6faf8497700c84c755001a9d
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 19:25:46 +0200

    2

--- ex2/src/WX2.c
+++ ex2/src/WX2.c
@@ -6,3 +6,17 @@
  */
 
 
+#include <stdio.h>
+
+void main()
+{
+	char c;
+
+	printf("Enter an alphabet: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%c",&c);
+    if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c='O'||c='u'||c=='U')
+    printf("%c is a vowel");
+    else
+    printf("%C is a constant");
+}
