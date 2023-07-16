974660ac66bc42d6369fbbc85659893487a5bebf
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 19:01:19 +0200

    5

--- assignment1.ex1/src/ex5.c
+++ assignment1.ex1/src/ex5.c
@@ -6,3 +6,13 @@
  */
 
 
+#include "stdio.h"
+
+void main()
+{
+	char c;
+	printf("Enter a character: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%c",&c);
+	printf("ASCII value of %c = %d",c,c);
+}
