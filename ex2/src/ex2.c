23f5a635478ac8b79d1d3550673be9ad8c18e970
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 18:39:25 +0200

    second commit

---
+++ ex2/src/ex2.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : ex2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+
+void main()
+{
+	int x;
+	printf("Enter a integer: ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+}
