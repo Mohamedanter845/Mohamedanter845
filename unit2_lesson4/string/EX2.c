9b0917e57cd877ac0fdf59b202e8891028cf6581
Anter <Anter@DESKTOP-9EVQA41>
2023-07-17 00:18:47 +0200

    2

--- unit2_lesson4/string/EX2.c
+++ unit2_lesson4/string/EX2.c
@@ -6,3 +6,16 @@
  */
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	char a[100];
+	printf("Enter the sentence:");
+	fflush(stdin);fflush(stdout);
+	gets(a);
+	printf("the lenght of  %s is %d",a,strlen(a));
+	return(0);
+
+}
