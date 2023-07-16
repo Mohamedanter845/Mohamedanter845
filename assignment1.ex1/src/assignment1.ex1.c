4377887043a12dc825342b9516e78e9f4df8edaa
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 18:31:00 +0200

    first commit

---
+++ assignment1.ex1/src/assignment1.ex1.c
@@ -0,0 +1,13 @@
+/*
+ * EX1.c
+ *
+ *  Created on: 1 july 2023
+ *      Author: mohamed anter
+ */
+#include <stdio.h>
+#include <stdio.lb>
+int main(void)
+{
+    printf("welcome to our embedded comunity");
+
+}
