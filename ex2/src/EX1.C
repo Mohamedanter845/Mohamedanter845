1b5e9a8eff8be607d6f14a0c60ecc4f6f4da26ed
Anter <Anter@DESKTOP-9EVQA41>
2023-07-16 19:22:26 +0200

    1

--- ex2/src/EX1.C
+++ ex2/src/EX1.C
@@ -6,3 +6,16 @@
  */
 
 
+#include <stdio.h>
+
+void main()
+{
+	int num;
+	printf("Enter an integer : ");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d",&num);
+	if(num%2==0)
+		printf("%d is even.",num);
+	else
+		printf("%d is odd.",num);
+}
