80f1c85e32edf0863ad97f17c2f918510d4c30ff
Anter <Anter@DESKTOP-9EVQA41>
2023-07-17 00:46:18 +0200

    3

--- unit2_lesson4/string/EX3.c
+++ unit2_lesson4/string/EX3.c
@@ -6,3 +6,22 @@
  */
 
 
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char arr[100],ch,temp;
+	int i,rev;
+	printf("enter the string:");
+	fflush(stdin);fflush(stdout);
+	gets(arr);
+	rev= strlen(arr)-1;
+	for (i=0 ;i<rev ;i++)
+	{
+		temp=arr[i];
+		arr[i]=arr[rev];
+		arr[rev]=temp;
+		rev--;
+	}
+	printf("reverse string is : %s",arr);
+}
