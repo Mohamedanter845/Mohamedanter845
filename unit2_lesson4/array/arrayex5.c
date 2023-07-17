b68ec1212beb3599ff7b941686ad6fea71eb4881
Anter <Anter@DESKTOP-9EVQA41>
2023-07-18 00:43:02 +0200

    5

--- unit2_lesson4/array/arrayex5.c
+++ unit2_lesson4/array/arrayex5.c
@@ -6,3 +6,41 @@
  */
 
 
+#include<stdio.h>
+int main(){
+	int x =0 ;
+		int searchnum =0;
+		int loc =0 ;
+		int arr[x];
+
+		printf("Enter no of elements: ");
+		fflush(stdin);fflush(stdout);
+		scanf("%d",&x);
+
+		for (int i=0 ; i<x;i++){
+				fflush(stdin);fflush(stdout);
+				scanf("%d",&arr[i]);
+
+			}
+			for (int i=0 ; i<x ;i++){
+
+				printf("%d\t",arr[i]);
+			}
+				printf("\n");
+				printf("Enter the elements to be searched : ");
+				fflush(stdin);fflush(stdout);
+				scanf("%d",&searchnum);
+				for(int i = 0 ; i<x ; i++){
+					if(arr[i] == searchnum)
+						loc = i+1;
+				}
+				printf("Number found at the location = %d",loc);
+
+
+			}
+
+
+
+
+
+
