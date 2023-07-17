837991894b33fa1d76105d969d66bd9e4e2c1b8c
Anter <Anter@DESKTOP-9EVQA41>
2023-07-17 15:46:05 +0200

    2

--- unit2_lesson4/array/arrayex2.c
+++ unit2_lesson4/array/arrayex2.c
@@ -6,3 +6,25 @@
  */
 
 
+#include<stdio.h>
+int main()
+{
+	int i=0,n=0;
+	float arr[n] ,sum=0 ,avr=0;
+	printf("enter the numbers of data: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&n);
+
+	for(i=0;i<n;i++){
+		printf("enter number%d:",i+1);
+
+		fflush(stdin);fflush(stdout);
+		scanf("%f",&arr[i]);
+	}
+	for(i=0;i<n;i++){
+
+	sum=sum+arr[i];
+	}
+	avr=sum/n;
+	printf("average=%.2f",avr);
+}
