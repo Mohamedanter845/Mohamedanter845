bb052b8bb7fd4baf4886d0a3bc703b0a277b6f64
Anter <Anter@DESKTOP-9EVQA41>
2023-08-04 17:18:20 +0200

    2

--- functions assignment/src/ex2-factorial_using_recrusion.c
+++ functions assignment/src/ex2-factorial_using_recrusion.c
@@ -1,8 +1,29 @@
 /*
- * ex2-factorial_using_recrusion.c
+*
+ * EX2_factorial.c
  *
- *  Created on: Aug 4, 2023
+ *  Created on: Aug 3, 2023
  *      Author: Anter
  */
 
+
+#include<stdio.h>
+int factorial(int x);
+int main(){
+	int n;
+	printf("enter  a positive number :");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&n);
+	printf("factorial of %d is %d",n,factorial(n));
+}
+int factorial(int x){
+	static int fact=1;
+	if(x>1)
+	{
+		fact*=x;
+		x--;
+		factorial(x);
+	}
+	return fact;
+}
 
